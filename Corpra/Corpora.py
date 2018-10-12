d883f70965b674453a4095430b00c83b4ffaf5a7
MMJ744 <MMJ744@cs.bham.ac.uk>
2018-10-12 16:26:06 +0100

    edited corpra

--- Corpra/Corpora.py
+++ Corpra/Corpora.py
@@ -1,13 +1,7 @@
 import nltk
-from nltk.corpus import brown
 from nltk.corpus.reader import WordListCorpusReader
 reader = WordListCorpusReader('', ['computerscience.txt'])
 words = [nltk.word_tokenize(i) for i in reader.words()]
-words2 = []
-for s in reader.words():
-    for i in s:
-        words2.append(i)
-tokens = [nltk.word_tokenize(i) for i in words2]
 from nltk.stem.porter import *
 stemmer = PorterStemmer()
 from nltk.stem import WordNetLemmatizer
@@ -16,8 +10,4 @@ lemmer = WordNetLemmatizer()
 stemmed = [[stemmer.stem(y) for y in i] for i in words]
 lemmed = [[lemmer.lemmatize(y) for y in i] for i in words]
 
-
-stemmed2 = [stemmer.stem(i) for i in tokens]
-lemmed2 = [lemmer.lemmatize(i) for i in tokens]
-
-print(stemmed2)
+print(stemmed)
