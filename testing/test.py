16a58936245e099fa4db45276b2428c8edcb0b71
Matthew Jones <mmj744@cs.bham.ac.uk>
2018-10-31 10:51:21 +0000

    minor edits

--- testing/test.py
+++ testing/test.py
@@ -1,8 +1,9 @@
 import nltk
-sentence = "Matthew went to the beach in Blackpool with his dog and Brett. James eats dragons with Gimly."
-tokens = nltk.word_tokenize(sentence)
-print(tokens)
 
+def backoff_tagger(train_sents, tagger_classes, backoff=None):
+    for cls in tagger_classes :
+        backoff = cls(train_sents, backoff=backoff)
+    return backoff
 
 from nltk.corpus import treebank
 from nltk.tag import DefaultTagger
@@ -11,18 +12,14 @@ test_set = treebank.tagged_sents()[2000:]
 from nltk.tag import UnigramTagger
 unigramTagger = UnigramTagger(train_set)
 from nltk.tag import BigramTagger, TrigramTagger
-bigramTagger = BigramTagger(train_set, cutoff=2)
+#bigramTagger = BigramTagger(train_set, cutoff=2)
 trigramTagger = TrigramTagger(train_set, cutoff=3)
-
-
-def backoff_tagger(train_sents, tagger_classes, backoff=None):
-    for cls in tagger_classes :
-        backoff = cls(train_sents, backoff=backoff)
-    return backoff
-
-
 tagger = backoff_tagger(train_set, [UnigramTagger, BigramTagger, TrigramTagger], backoff=DefaultTagger('NN'))
-
+print(tagger.evaluate(test_set))
+          
+sentence = "John Smith went to the beach in Blackpool with his dog and Brett. James eats dragons with Gimly."
+tokens = nltk.word_tokenize(sentence)
+print(tokens)
 taggedTokens = tagger.tag(tokens)
 print(taggedTokens)
 tree = nltk.ne_chunk(taggedTokens, binary=False)
